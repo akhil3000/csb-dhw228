81a46cbd0343a3cd279ed1a0b068eea214490991
Code Sandbox <hello@codesandbox.io>
2023-03-06 06:39:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9ea0e839baa3c5e32f5a19b268a013ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4393836ca53ea82e87f9098767c8cac4",
+    "url": "https://akhil3000.github.io/csb-dhw228/index.html"
+  },
+  {
+    "revision": "465b23b03010b3410c59",
+    "url": "https://akhil3000.github.io/csb-dhw228/static/js/2.8dfa08cb.chunk.js"
+  },
+  {
+    "revision": "5abd678cbb55d9f10d95",
+    "url": "https://akhil3000.github.io/csb-dhw228/static/js/main.3947ff37.chunk.js"
+  },
+  {
+    "revision": "07a714c57236f6499883",
+    "url": "https://akhil3000.github.io/csb-dhw228/static/js/runtime-main.a64c7276.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://akhil3000.github.io/csb-dhw228/precache-manifest.9ea0e839baa3c5e32f5a19b268a013ad.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://akhil3000.github.io/csb-dhw228/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
